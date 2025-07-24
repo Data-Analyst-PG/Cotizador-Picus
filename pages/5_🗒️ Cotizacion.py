908ef7ee810b5c6325721200b0f5b1a6eba4d6e4
Data-Analyst-PG <data.analyst@palosgarza.com>
2025-07-24 11:17:07 -0500

    Update 5_🗒️ Cotizacion.py

--- pages/5_🗒️ Cotizacion.py
+++ pages/5_🗒️ Cotizacion.py
@@ -115,7 +115,7 @@ if st.button("Generar Cotización PDF"):
 
     class PDF(FPDF):
         def header(self):
-            self.image('Cotización Picus.png', x=0, y=0, w=215.9, h=279.4)
+            self.image('Cotización Picus.png', x=0, y=0, w=8.5, h=11)
 
     pdf = PDF(orientation='P', unit='in', format='Letter')
     pdf.alias_nb_pages()
